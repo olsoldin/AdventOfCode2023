09e86c695b3efa418ff8999f4092831bbcd8c4b6
Oliver <oliver.olding@hotmail.com>
2024-07-23 23:26:46 +0100

    initial day9

---
+++ tests/Day9Test.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day9Test {
+
+	private Day9 day9;
+
+	@BeforeEach
+	void setUp() {
+		day9 = new Day9();
+	}
+}
